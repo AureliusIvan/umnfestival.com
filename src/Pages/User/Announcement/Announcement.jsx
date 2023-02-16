a93ed86a0cda1c86da57b50dd5ab5d4cd428121c
AureliusIvan <aurelius.ivan@student.umn.ac.id>
2023-02-16 22:13:54 +0700

    current

--- src/Pages/User/Announcement/Announcement.jsx
+++ src/Pages/User/Announcement/Announcement.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Pilar from '../../../Reusable/ComponentItems/Pilar/Pilar';
+import AnnouncementCard from './Components/AnnouncementCard';
 import "./Announcement.scss";
+import File from "./File/pengumuman.pdf";
+
+function Content() {
+    return (
+        <div className='Content'>
+            <div className='Content-Title'>
+                <img className='image' />
+                {/* <img src={} */}
+            </div>
+        </div>
+    )
+}
 
 function Announcement() {
     return (
         <div className='Announcement'>
             <Pilar />
-            <h1 className='Title'>Announcement</h1>
+            <h1 className='Title'>
+                Annoucement
+            </h1>
+            <Content />
         </div>
     )
 }

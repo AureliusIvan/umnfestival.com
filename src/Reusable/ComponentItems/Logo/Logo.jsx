4f4ca47c9922e3cc581e1b71b60416a1dc184215
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-04-21 15:13:13 +0700

    F0001 - Responsive Ufest Logo
    commit pertama, cuman test dan masukin image

---
+++ src/Reusable/ComponentItems/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import Logo from "../../../Asset/Image/Ufest Logo/ufestlogowhite.webp"
+
+export default function Logo() {
+    return (
+            <div>
+                <img src={Logo} alt="" />
+            </div>
+    )
+}

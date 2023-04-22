61846e50090f05c76392eb607e7ea42c43deca69
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-04-22 12:02:27 +0700

    F0001-Responsive Ufest Logo
    Ubah Routing, nambahin logo dan buat logo responsive

--- src/Reusable/ComponentItems/Logo/Logo.jsx
+++ src/Reusable/ComponentItems/Logo/Logo.jsx
@@ -1,9 +1,12 @@
-import Logo from "../../../Asset/Image/Ufest Logo/ufestlogowhite.webp"
+//pake logo yang berwarna karena logo putih ngak keliatan
+//ubah setelah selesai
+import LogoUfest from "../../../Asset/Image/Ufest Logo/ufestlogocolor.webp"
+import "./Logo.scss"
 
 export default function Logo() {
     return (
             <div>
-                <img src={Logo} alt="" />
+                <img className="LogoResponsive" src={LogoUfest} alt="" />
             </div>
     )
 }
--- src/Route/Routing.jsx
+++ src/Route/Routing.jsx
@@ -3,11 +3,21 @@ import ProtectedRoute from "./Components/ProtectedRoute"
 import { lazy } from "react"
 import { Suspense } from "react";
 import LoadingScreen from "../Reusable/LoadingScreen/LoadingScreen";
+import Logo from "../Reusable/ComponentItems/Logo/Logo";
 const User = lazy(() => import("../Pages/User/User"));
 const Admin = lazy(() => import("../Pages/Admin/Admin"));
 
+//Nambah Component yang lagi dibuat
+//import Logo from "../Reusable/ComponentItems/Logo";
+
 export default function Routing() {
     return (
+        <Routes>
+                <Route path="/*" element={<Logo />} />
+        </Routes>
+
+
+        /*
         <Routes>
             <Route path="/*" element={
                 <Suspense fallback={<LoadingScreen />}>
@@ -22,6 +32,6 @@ export default function Routing() {
             </>
             } />
             <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        </Routes>*/
     )
 }
